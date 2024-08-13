d8d0a41c4607bc8430dc9aa136360cc36fd9b565
alinashr <108988055+alinashr@users.noreply.github.com>
2024-08-13 21:09:36 +0545

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,11 @@
+# from django.contrib import admin
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('', home),
+    path('contact', contact),
+    path('services', services)
+]
---
+++ app1/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request,"homepage.html")
+def contact(request):
+    return render(request,"contact.html")
+def services(request):
+    return render(request,"services.html")
